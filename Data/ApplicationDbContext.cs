bba47c424d33b7e6fe2b7460920c7927011912c6
Samara Rosario <samara48240196@edu.pa.senac.br>
2023-09-01 17:52:56 -0300

    Adicionar arquivos de projeto.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplicationExemplo1.Data {
+    public class ApplicationDbContext : DbContext {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Produto> Produto { get; set; } = default!;
+    }
+}
